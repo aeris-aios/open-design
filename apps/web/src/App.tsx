9cba0a7288f47f33c39d8f981395782eb427f295
Siri-Ray <2667192167@qq.com>
2026-08-17 22:38:25 +0800

    fix project update-ready rocket

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -56,6 +56,7 @@ import { AmrArtifactUpgradeGate } from './components/AmrArtifactUpgradeGate';
 import { AmrArtifactUpgradeHomeCard } from './components/AmrArtifactUpgradeHomeCard';
 import { TooltipLayer } from './components/TooltipLayer';
 import { UpdateDialog } from './components/UpdateDialog';
+import { UpdaterPopup } from './components/UpdaterPopup';
 import {
   openWorkspaceTab,
   removeWorkspaceProjectTabs,
@@ -5297,6 +5298,13 @@ function AppInner() {
           <WorkspaceTopRightAccountCluster
             onOpenSettings={openSettings}
             onSignedOut={handleActiveCloudSignOut}
+            updaterSlot={
+              <UpdaterPopup
+                allowSilentUpdates={config.allowSilentUpdates}
+                silentUpdatePreferenceReady={daemonAppConfigReady}
+                onAllowSilentUpdatesChange={handleSilentUpdatePreferenceChange}
+              />
+            }
             workspaceContextOverride={
               activeProject?.workspaceId
                 ? activeProjectWorkspaceContext
