c18e3095e96a5bd0ed75d4d9737f423ab0e01f1e
lefarcen <935902669@qq.com>
2026-08-31 11:13:42 +0800

    fix nix dependency hashes

--- nix/pnpm-deps.nix
+++ nix/pnpm-deps.nix
@@ -9,6 +9,6 @@
   # 1. Temporarily set the consuming `hash = lib.fakeHash;`
   # 2. Run the relevant nix build/flake check
   # 3. Copy the expected hash printed by Nix into the matching field below
-  daemonHash = "sha256-ujXjphrP9TvyLvIFaiplOfWMhQEbMsy74KkCmqALkus=";
-  webHash = "sha256-OfTksoOcxLu6OairFxQEYLQomj1JFCDEA3maKiH+9bQ=";
+  daemonHash = "sha256-fHB2diLIQHGlp9xaUuPkm6oDggGp7GOJTckL3hA2/Bc=";
+  webHash = "sha256-t1a6SwJYBtxlkckWiw8FkCH1tuJmluShcg/e7d4bzcQ=";
 }
